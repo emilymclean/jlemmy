198e40965c2d3cbec34d2b46799ff1b2e842d25b
Ben McLean <ben.mclean@stripysock.com.au>
2024-05-10 19:53:36 +1000

    Remove unused code from enum generator

--- type-generator/src/generator/enum_generator.py
+++ type-generator/src/generator/enum_generator.py
@@ -37,24 +37,3 @@ public enum {self._enum_name} {{
                 """.strip())
 
         return ",\n".join(lines)
-
-    def _generate_constructor(self) -> str:
-        return f"""
-public {self._class_name}({", ".join([f"{prop.type} {prop.java_name}" for prop in self._properties])}) {{
-{textwrap.indent(self._generate_constructor_assignments(), self._indent_char)}
-}}
-            """.strip()
-
-    def _generate_constructor_assignments(self) -> str:
-        return "\n".join([f"this.{prop.java_name} = {prop.java_name}" for prop in self._properties])
-
-    def _generate_get_and_sets(self) -> str:
-        return "\n\n".join([f"""
-public {prop.type} get{prop.cap_java_name}() {{
-    return {prop.java_name};
-}}
-
-public void set{prop.cap_java_name}({prop.type} {prop.java_name}) {{
-    this.{prop.java_name} = {prop.java_name};
-}}
-            """.strip() for prop in self._properties])
